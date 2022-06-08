e43a1389635e9b488a3692a78898ed8da094676f
mshyd925 <102269158+mshyd925@users.noreply.github.com>
2022-06-08 09:50:48 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "12.0.0.0/16"
+  cidr_block       = "20.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "demo-vpc"
+    Name = "demo-vpc-hyd"
     Purpose = "Jenkins Demo"
   }
 }
